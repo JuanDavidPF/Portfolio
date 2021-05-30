14471c300ec4918b2988133df8403a3577e16e00
Juan David Payán <JuanDavidPF@gmail.com>
2021-05-30 15:27:18 -0500

    Added firebase to project

---
+++ source/Firebase.js
@@ -0,0 +1,13 @@
+ 
+  var firebaseConfig = {
+    apiKey: "AIzaSyA91MEUlxHIXlPnkigIs60cE_alLoUF0bI",
+    authDomain: "portfolio-e3369.firebaseapp.com",
+    projectId: "portfolio-e3369",
+    storageBucket: "portfolio-e3369.appspot.com",
+    messagingSenderId: "913562573933",
+    appId: "1:913562573933:web:e4eb11f8ce4cc813d0f364",
+    measurementId: "G-W6R2257V74"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
